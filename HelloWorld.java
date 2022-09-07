72c5b54b91c4da8d502fb1dc4bac04527780035e
JavierOsornio <112905913+JavierOsornio@users.noreply.github.com>
2022-09-07 16:15:06 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println(".......******me changes******.......");
+		System.out.println(".......******Please go******.......");
 	}
 }
