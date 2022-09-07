3f3310bb519642f0878f0640ab81bb31abb9f2c2
JavierOsornio <112905913+JavierOsornio@users.noreply.github.com>
2022-09-07 15:28:55 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println(".......******Hello world and welcome to Jenkins training******.......");
+		System.out.println(".......******Made some changes******.......");
 	}
 }
